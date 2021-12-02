07abac03d67ea3e54981f6c07512af76611b4a6c
HeeYoung-Youn <yhy1@naver.com>
2021-12-02 14:08:14 +0900

    Add: use-react-router package
    use-react-router package 사용 시 history, location, match 쉽게 사용 가능

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
+import RouterHookSample from "./RouterHookSample";
 
 const Profiles = () => {
   return (
@@ -31,6 +32,7 @@ const Profiles = () => {
         render={() => <div>사용자를 선택해주세요</div>}
       />
       <Route path="/profiles/:username" component={Profile} />
+      <RouterHookSample />
     </div>
   );
 };
